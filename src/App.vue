06e8ccf942b2e12efc10a4e80da827ee93af2aab
MotNam <motokonamba@gmail.com>
2022-03-14 17:51:34 +0100

    lab except it 4

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- iteration 1 -->
+    <p>New Component</p>
+    <NewVueComp />
+    
+<!-- 　　iteration 2  -->
+<span>Message:{{message}} </span>
+    <!-- iteration 3 -->
+<button @click = "printFunction">Click Here</button> 
+
+<!-- iteration 4 -->
+<div>
+<a href="/views/AboutView.vue"> About Page</a><br>
+
+<a href="/views/HomeView.vue"> Home Page</a>
+</div>
+  </div>
+
+</template>
+
+<script>
+import NewVueComp from './components/NewVueComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NewVueComp,
+  },
+   data() {
+    return {
+      message:'Text Interpolation',
+  }
+  },
+   methods: {
+      printFunction(){
+        console.log("Hello World");
+      },
+    
+}
+}
+
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/NewVueComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>First Step for Lab Wk4 Day1</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
